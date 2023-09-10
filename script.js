b9a06142a2363838ce19b19b2437683924fd44a2
Siva Prasad K <122608989+kvdsp14@users.noreply.github.com>
2023-09-10 11:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	for (let i in sampleObject) {
+	let flag = false
+  for (let i in sampleObject) {
     if (i === key) {
-      return ture;
-      
+      flag = true
     }
-    else {
-		
-      return false;
-	}
-    
   }
-}
+  return flag;
 
+
+}
+	
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
